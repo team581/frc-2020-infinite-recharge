01c04777efbfd419b1e2472a2a5b726c6958e8aa
Jonah Snider <jonah@jonah.pw>
2020-02-24 20:38:29 -0800

    style(auto): format
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/main/java/club/team581/util/auto/Autonomous.java
+++ src/main/java/club/team581/util/auto/Autonomous.java
@@ -17,36 +17,38 @@ import edu.wpi.first.wpilibj.controller.PIDController;
  * Add your docs here.
  */
 public class Autonomous {
-    static TalonFX[] allMotors = RobotContainer.driveSubsystem.allMotors;
-    static double initalAverageEncoderValue = 0;
-    static double currentEncoderValue = 0;
-    static double encoderDistance = 0;
-    static double distanceToDrive = 0;
-    static double drivingAdjust = 0;
-    static double marginOfError = 0;
-    static double divisor = 0;
-    public final static PIDController driveController = new PIDController(0, 0, 0);
+  static TalonFX[] allMotors = RobotContainer.driveSubsystem.allMotors;
+  static double initalAverageEncoderValue = 0;
+  static double currentEncoderValue = 0;
+  static double encoderDistance = 0;
+  static double distanceToDrive = 0;
+  static double drivingAdjust = 0;
+  static double marginOfError = 0;
+  static double divisor = 0;
+  public final static PIDController driveController = new PIDController(0, 0, 0);
 
-    public static final double getDriveCommand(final double initalEncoderValue, final double distanceToDrive) {
-        /** Gets the current average encoder value of the motors */
-        for (int i = 0; i < allMotors.length; i++) {
-            currentEncoderValue = currentEncoderValue + allMotors[i].getSelectedSensorPosition();
-        }
-        currentEncoderValue = currentEncoderValue / allMotors.length;
-
-        /** Finds the distance traveled after starting the command */
-        encoderDistance = (currentEncoderValue - initalEncoderValue)
-                / Constants.Autonomous.Movement.INCHES_PER_ROTATION;
-
-        /** Calculates the output of the PID controller */
-        drivingAdjust = driveController.calculate(encoderDistance, distanceToDrive) / divisor;
-
-        return drivingAdjust;
+  public static final double getDriveCommand(final double initalEncoderValue, final double distanceToDrive) {
+    /** Gets the current average encoder value of the motors */
+    for (int i = 0; i < allMotors.length; i++) {
+      currentEncoderValue = currentEncoderValue + allMotors[i].getSelectedSensorPosition();
     }
+    currentEncoderValue = currentEncoderValue / allMotors.length;
 
-    public static final boolean isFinished() {
-        /** Checks if the distance to the desired encoder value is within a margin of error */
-        return distanceToDrive - marginOfError < encoderDistance && encoderDistance < distanceToDrive + marginOfError;
+    /** Finds the distance traveled after starting the command */
+    encoderDistance = (currentEncoderValue - initalEncoderValue) / Constants.Autonomous.Movement.INCHES_PER_ROTATION;
 
-    }
+    /** Calculates the output of the PID controller */
+    drivingAdjust = driveController.calculate(encoderDistance, distanceToDrive) / divisor;
+
+    return drivingAdjust;
+  }
+
+  public static final boolean isFinished() {
+    /**
+     * Checks if the distance to the desired encoder value is within a margin of
+     * error
+     */
+    return distanceToDrive - marginOfError < encoderDistance && encoderDistance < distanceToDrive + marginOfError;
+
+  }
 }
